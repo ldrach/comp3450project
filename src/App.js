5dc827cca5fb24eb6945afd80d7b45536898e2aa
lyndo <lyndondrach@gmail.com>
2020-11-04 13:14:09 -0800

    changed link destinations in navbar

--- src/App.js
+++ src/App.js
@@ -7,23 +7,18 @@ import Login from "./login/login";
 import Signup from "./login/signup";
 import Forgot from "./login/forgot";
 import Home from "./home/home.js";
-import NavbarComponent from "./navbar/navbar";
 
-function displayNavBar() {
-    return (<NavbarComponent />);
-}
+
 
 function App() {
   return (
       <Router>
-
-          <br />
           <div className="container">
             <Route path="/" exact component={Login} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <Route path="/forgot" component={Forgot} />
-            <Route path="/home" component={Home} render={ () => displayNavBar()} />
+            <Route path="/home" component={Home} />
           </div>
       </Router>
   );
--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -6,11 +6,11 @@ export default class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">MovieFinder</Link>
+                <Link to="/home" className="navbar-brand">MovieFinder</Link>
                 <div className="collpase navbar-collapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="navbar-item">
-                            <Link to="/" className="nav-link">Home</Link>
+                            <Link to="/home" className="nav-link">Home</Link>
                         </li>
                         <li className="navbar-item">
                             <Link to="/trending" className="nav-link">Trending</Link>
