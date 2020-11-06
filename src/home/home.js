c417113e3452fb6e2d0744d27356759f1a36bb4c
gtanda <govind_tanda1@hotmail.com>
2020-11-06 15:41:27 -0800

    Fixed broken link error where images were null, no longer displaying posters if null

--- src/home/home.js
+++ src/home/home.js
@@ -1,11 +1,11 @@
 import React, {Component} from "react";
 import Navbar from "../navbar/navbar";
+const axios = require('axios').default;
 
 // AXIOS FOR HTTP REQUEST, BASEURL AND API KEY FOR TMBD
-const axios = require('axios').default;
 const BASEURL = 'https://api.themoviedb.org/3/';
 const APIKEY = '5cc7ef858dfde24d7396645c83fbacb6';
-const SIZE = "w92";
+const SIZE = "w185";
 const BASEIMGURL = "https://image.tmdb.org/t/p"
 
 export default class MovieSearch extends Component {
@@ -25,13 +25,13 @@ export default class MovieSearch extends Component {
         if (event.keyCode === 13) {
             axios.get(`${BASEURL}search/movie?api_key=${APIKEY}&query=${event.target.value}`)
                 .then(response => {
+                    console.log(response.data.results);
                     this.setState({
                         movies: response.data.results.map(movie => ({
                             title: movie.title,
-                            popularity: movie.popularity,
-                            poster: movie.poster_path,
-                            overview: movie.overview,
-                            moveID: movie.id,
+                            poster: movie.poster_path == null ? null : movie.poster_path,
+                            overview: movie.overview == null ? null : movie.overview,
+                            key: movie.id
                         }))
                     })
                 })
@@ -44,11 +44,11 @@ export default class MovieSearch extends Component {
     render() {
         let movie = this.state.movies;
         console.log(movie);
-        console.log(movie.length);
+
         const style = {
-            backgroundColor: 'white',
-            border: '1px solid black',
-            margin: '2vh auto'
+            border: '3px solid white',
+            margin: '2vh 1.5vh',
+            display: 'inline-block'
         }
 
         const inputStyle = {
@@ -57,26 +57,24 @@ export default class MovieSearch extends Component {
             color: 'black',
             borderRadius: '5px'
         }
+
         return (
-            <div>
+            <div className="container-fluid">
                 <Navbar/>
                 <div style={inputStyle}>
                     <label htmlFor="search">Search Movie: </label>
                     <input type="text" id="search" style={inputStyle} onKeyDown={event => this.clickHandler(event)}/>
                 </div>
-                <secion className="container-fluid section">
-
+                <section className="container-fluid section">
                     {/* MAP MOVIES FROM STATE TO A DIV, POSSIBLY CAN BE MOVED TO A "MOVIE CONTAINER" COMPONENT INSTEAD OF
                     HAVING ALL THIS GARBAGE HERE */}
-
                     {movie.length > 1 ? movie.map(item =>
-                        <div style={style}>
-                            <h1 key={item.movieID}>{item.title}</h1>
-                            <h1 key={item.movieID}>{item.popularity}</h1>
-                            <img src={`${BASEIMGURL}/${SIZE}/${item.poster}`} alt="Movie Poster Image"/>
-                            <p key={item.movieID}>{item.overview}</p>
-                        </div>) : null}
-                </secion>
+                        item.poster == null ? null :
+                            <div style={style}>
+                                <img src={`${BASEIMGURL}/${SIZE}/${item.poster}`}/>
+                            </div>
+                        ) : null}
+                </section>
             </div>
         );
     }
