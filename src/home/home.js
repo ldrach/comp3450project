1f92413b3b2b6a99139f93f355013eb9daceb0e0
gtanda <govind_tanda1@hotmail.com>
2020-11-05 16:27:01 -0800

    Small fix to remove white space

--- src/home/home.js
+++ src/home/home.js
@@ -5,8 +5,8 @@ import Navbar from "../navbar/navbar";
 const axios = require('axios').default;
 const BASEURL = 'https://api.themoviedb.org/3/';
 const APIKEY = '5cc7ef858dfde24d7396645c83fbacb6';
-const size = "w92";
-const baseImgUrl = "https://image.tmdb.org/t/p"
+const SIZE = "w92";
+const BASEIMGURL = "https://image.tmdb.org/t/p"
 
 export default class MovieSearch extends Component {
 
@@ -20,29 +20,31 @@ export default class MovieSearch extends Component {
         }]
     }
 
-
+    // On enter, send query to API thru axios, set state
     clickHandler = (event) => {
         if (event.keyCode === 13) {
-            const query = event.target.value;
-            axios.get(`${BASEURL}search/movie?api_key=${APIKEY}&query=${query}`)
+            axios.get(`${BASEURL}search/movie?api_key=${APIKEY}&query=${event.target.value}`)
                 .then(response => {
-                    console.log(response);
                     this.setState({
                         movies: response.data.results.map(movie => ({
                             title: movie.title,
                             popularity: movie.popularity,
                             poster: movie.poster_path,
                             overview: movie.overview,
-                            moveID: movie.id
+                            moveID: movie.id,
                         }))
                     })
                 })
+        event.target.value = ''; // Empty search input
         }
     }
 
 
+
     render() {
         let movie = this.state.movies;
+        console.log(movie);
+        console.log(movie.length);
         const style = {
             backgroundColor: 'white',
             border: '1px solid black',
@@ -58,19 +60,22 @@ export default class MovieSearch extends Component {
         return (
             <div>
                 <Navbar/>
-
                 <div style={inputStyle}>
                     <label htmlFor="search">Search Movie: </label>
                     <input type="text" id="search" style={inputStyle} onKeyDown={event => this.clickHandler(event)}/>
                 </div>
                 <secion className="container-fluid section">
-                    {movie.map(item =>
+
+                    {/* MAP MOVIES FROM STATE TO A DIV, POSSIBLY CAN BE MOVED TO A "MOVIE CONTAINER" COMPONENT INSTEAD OF
+                    HAVING ALL THIS GARBAGE HERE */}
+
+                    {movie.length > 1 ? movie.map(item =>
                         <div style={style}>
                             <h1 key={item.movieID}>{item.title}</h1>
                             <h1 key={item.movieID}>{item.popularity}</h1>
-                            <img src={`${baseImgUrl}/${size}/${item.poster}`} alt=""/>
+                            <img src={`${BASEIMGURL}/${SIZE}/${item.poster}`} alt="Movie Poster Image"/>
                             <p key={item.movieID}>{item.overview}</p>
-                        </div>)}
+                        </div>) : null}
                 </secion>
             </div>
         );
