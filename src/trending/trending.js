c52de02ce9f01d050bf39561c567b6d0f12894c2
lyndo <lyndondrach@gmail.com>
2020-11-10 13:35:31 -0800

    started trending

---
+++ src/trending/trending.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Navbar from "../navbar/navbar";
+
+
+export default class Trending extends Component {
+
+    render() {
+        return (
+            <Navbar />
+
+        );
+    }
+}
