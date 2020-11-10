eee0a61f3d037f21bea1f3e49d20316dd00a759b
lyndo <lyndondrach@gmail.com>
2020-11-10 09:41:26 -0800

    started Friends page

--- src/friends/friends.js
+++ src/friends/friends.js
@@ -1,15 +1,19 @@
 import React from "react";
-
+import Navbar from "../navbar/navbar";
+import "./friends.css"
 
 const Friends = () => {
     return (
-        <div>
-            <ul>
-                {["Alex", "James", "Andy", "Beth", "Matt"].map((user, idx)=> {
-                    return <li key={idx}>{user}</li>
-                })}
-            </ul>
+        <container>
+            <Navbar/>
+            <div className="upperContainer">
+                <ul>
+                    {["Alex", "James", "Andy", "Beth", "Matt"].map((user, idx)=> {
+                        return <li key={idx}>{user}</li>
+                    })}
+                </ul>
         </div>
+        </container>
     );
 };
 
