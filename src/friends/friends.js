22d506886c9267d8143ee49126ad60138b6f74a6
lyndo <lyndondrach@gmail.com>
2020-11-14 10:13:48 -0800

    Started working to import friends list from db
    Merged Jordans Branch to Lyndons branch (code was missing from jordans branch)

--- src/friends/friends.js
+++ src/friends/friends.js
@@ -1,31 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
 import Navbar from "../navbar/navbar";
 import "./friends.css"
+import axios from "axios";
 
-const Friends = () => {
-    return (
-        <React.Fragment>
-            <Navbar/>
-            <h2 className="friendsTitle">My Friends</h2>
-            <div className="upperContainer">
-                <ul>
-                    {["Alex", "James", "Andy", "Beth", "Matt"].map((user, idx)=> {
-                        return <li key={idx}>{user}</li>
-                    })}
-                </ul>
-            </div>
-            <div className="inputDiv">
-                <input type="text" id="friendSearch" aria-label="Search Bar" placeholder="Find Friends"/>
-            </div>
-            <div className={"friendSearchDisplay"}>
-                <ul>
-                    {["Alex", "James", "Andy", "Beth", "Matt"].map((user, idx)=> {
-                        return <li key={idx}>{user}</li>
-                    })}
-                </ul>
-            </div>
-        </React.Fragment>
-    );
+
+
+export default class Friends extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar/>
+                <h2 className="friendsTitle">My Friends</h2>
+                <div className="upperContainer">
+                    <ul>
+                        {["Alex", "James", "Andy", "Beth", "Matt"].map((user, idx) => {
+                            return <li key={idx}>{user}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className="inputDiv">
+                    <input type="text" id="friendSearch" aria-label="Search Bar" placeholder="Find Friends"/>
+                </div>
+                <div className={"friendSearchDisplay"}>
+                    <ul>
+                        {["Alex", "James", "Andy", "Beth", "Matt"].map((user, idx) => {
+                            return <li key={idx}>{user}</li>
+                        })}
+                    </ul>
+                </div>
+            </React.Fragment>
+        );
+    }
 };
 
-export default Friends;
